910d51d56f5608e91954b7a1ee09ab67e1f41fb1
khushTailor409 <tailork.2008@gmail.com>
2025-12-13 18:06:26 -0500

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,41 @@
+package org.khush;
+
+import lombok.*;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode
+
+public class Department {
+
+    private String departmentId;
+    @Setter private String departmentName;
+    private static int nextId = 1;
+
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentName = departmentName;
+            this.departmentId = generateDepartmentId();
+        } else { this.departmentName = null;
+            this.departmentId = null;}
+    }
+    private static String generateDepartmentId() {
+        String id = String.format("D%02d", nextId);
+        nextId++;
+        return id;
+    }
+
+    public static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+        for (int i = 0; i < departmentName.length(); i++) { // check if all characters are letters or spaces
+            char c = departmentName.charAt(i);
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
